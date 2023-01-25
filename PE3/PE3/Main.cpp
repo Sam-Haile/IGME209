08f63c9aecd1f3d8522a08a4adcff268d53bc8c2
Sam-Haile <92647194+Sam-Haile@users.noreply.github.com>
2023-01-25 14:30:36 -0500

    PE3 Completed

---
+++ PE3/PE3/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Functions.h"
+
+using namespace std;
+
+
+int main() {
+	PrintName();
+}
+
+// Hello World Function
+void PrintName() {
+	cout << "Sam Haile" << endl;
+}
---
+++ PE3/PE3/functions.h
@@ -0,0 +1,2 @@
+//function decleration
+void PrintName();
