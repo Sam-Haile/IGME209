a9c5a104da3fa67b0755bb197287adda979863fa
Sam-Haile <92647194+Sam-Haile@users.noreply.github.com>
2023-01-27 15:41:03 -0500

    Restructure files
    fixed the file organization to work on one solution

--- DSA1/PE2/PE2.cpp
+++ DSA1/PE2/PE2.cpp
@@ -2,18 +2,18 @@
 #include <cmath>
 int main()
 {
-    printf ("Hello World!\n");
+    printf("Hello World!\n");
 
     // Determine the number of seconds in December
     int seconds;
     seconds = 31 * 24 * 60;
-    printf ("There are %i seconds in December!\n", seconds);
+    printf("There are %i seconds in December!\n", seconds);
 
     // Calculate the area of a circle
     double radius = 6.2f;
     float pi = 3.14159f;
     float area = pi * pow(radius, 2);
-    printf ("The area of a circle with radius of 6.2 is %f\n", area);
+    printf("The area of a circle with radius of 6.2 is %f\n", area);
 
     int a = 13;
     int b = 7;
--- DSA1/PE3/Main.cpp
+++ DSA1/PE3/Main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Functions.h"
+#include "PE3.h"
 
 using namespace std;
 
